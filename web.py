e0b46c834c4aba9b5cf920bae91a4aaa15d4a968
Dan Cigrang <danielcigrang@me.com>
2017-10-31 15:05:14 -0500

    app -> application

--- web.py
+++ web.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, Markup, request
 import cta
-app = Flask(__name__)
-# app.run(debug=True)
+application = Flask(__name__)
+# application.run(debug=True)
 
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     print request.headers.get('User-Agent').lower()
     data = cta.print_next_arrivals(30116, "Blue", "Forest Park")
     return render_template('ninja.html', data=Markup(data))
 
-@app.route('/<line>')
+@application.route('/<line>')
 def get_route(line):
     return render_template('ninja.html', data="CTA %s Line\n" % line)
 
-@app.route('/<line>/<station>')
+@application.route('/<line>/<station>')
 def get_station(line, station):
     return render_template('ninja.html', data="CTA %s Line, %s station" % (line, station))
