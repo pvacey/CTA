6ed449433df65810b57ea6c5123b73edaad71d8b
Dan Cigrang <danielcigrang@me.com>
2017-10-30 22:39:52 -0500

    commiting changes that i dont want to forget

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Markup, request
 import cta
 app = Flask(__name__)
-app.run(debug=True)
+# app.run(debug=True)
 
 
 @app.route('/')
@@ -9,3 +9,11 @@ def hello_world():
     print request.headers.get('User-Agent').lower()
     data = cta.print_next_arrivals(30116, "Blue", "Forest Park")
     return render_template('ninja.html', data=Markup(data))
+
+@app.route('/<line>')
+def get_route(line):
+    return render_template('ninja.html', data="CTA %s Line\n" % line)
+
+@app.route('/<line>/<station>')
+def get_station(line, station):
+    return render_template('ninja.html', data="CTA %s Line, %s station" % (line, station))
