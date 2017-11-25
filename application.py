bba086df65414506932b01d3e68ca06766889715
pvacey <vaks0731@gmail.com>
2017-11-24 18:00:11 -0600

    Add web routing & backend re-do (#13)
    * adding some help text to the functions
    
    * adding web template
    
    * web interface has concept of what i want
    
    * commiting changes that i dont want to forget
    
    * removing reddit file oops
    
    * eb
    
    * app -> application
    
    * rename to application
    
    * flask requirement
    
    * overhaul backend
    
    * tweak flask config, handle bad station name from user
    
    * added a terminal print lines function
    
    * GET / returns list of train lines
    
    * updated tests but they dont really do much yet
    
    * remove leading space
    
    * s

---
+++ application.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, Markup, request
+import cta
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return output(request, cta.get_term_lines())
+
+@application.route('/<line>')
+def get_line(line):
+    return output(request, cta.get_term_line_stations(line))
+
+@application.route('/<line>/<station>')
+def get_times(line, station):
+    return output(request, cta.get_term_arrivals(line_name=line,
+                                                 station_name=station))
+
+def output(requests, data):
+    """Decide how to present the data to the user based on user agent"""
+    user_agent = request.headers.get('User-Agent').lower()
+    if user_agent.startswith('curl'):
+        return data
+    else:
+        return '<pre>{}</pre>'.format(data)
+
+if __name__ == '__main__':
+    application.run(debug=True)
--- cta.py
+++ cta.py
@@ -1,87 +1,169 @@
 #!venv/bin/python
-import requests
+"""An efficient way to check your train schedule."""
 import json
 from datetime import datetime
-import ConfigParser
+import sys
 import yaml
-
-def printNextArrivals(station_id, line, dest):
-    with open("config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-
-    api_key =  cfg['api']['key']
-    # Red, Blue, G, Brn, P, Y Pnk, O
-    lines = getLines()
-    try:
-        resp = getArrivals(api_key, station_id)
-    except requests.exceptions.RequestException as e:  # This is the correct syntax
-        print e
-        sys.exit(1)
-    arrivals = []
-    trains = resp['ctatt']['eta']
-    for t in trains:
-        if t['rt'] == line and t['destNm'] == dest:
-            tmp = datetime.strptime(t['arrT'], "%Y-%m-%dT%H:%M:%S")
-            arrival = tmp.strftime("%I:%M:%S")
-            elapsed = (tmp - datetime.now()).seconds
-            minutes = elapsed / 60
-            seconds = elapsed % 60
-
-            arrivals.append({'time': arrival,
-                             'minutes': minutes,
-                             'seconds': seconds})
-            station_name = t['staNm']
-
-    response = '-' * 31
-    response += '\n'
-    response += '| CTA {} Line'.format(lines[line]) + ' ' * 15 + '|\n'
-    response += '-' * 31
-    response += '\n'
-    response += '{} ---> {}'.format(station_name, dest)
-    for a in arrivals:
-        response += '\n - {}m {}s from now ({})'.format(
-            a['minutes'], a['seconds'], a['time'])
-    response += '\n'
-    response += '-' * 31
-    return response
-
-
-def get_lines():
-    return {'red': 'Red', 'Blue': 'Blue', 'G': 'Green', 'Brn': 'Brown',
-    'P': 'Purple', 'Y': 'Yellow', 'Pnk': 'Pink', 'O': 'Orange'}
+import requests
 
 def get_raw_stations():
+    """Gets the raw station information"""
     return requests.get('https://data.cityofchicago.org/resource/8mj8-j3c4.json').json()
 
-def get_stops(line):
-    '''Returns list of stops for a given line'''
-    tmp = get_lines()
-    new_lines = dict(zip(tmp.values(), tmp.keys()))
-    station_code = new_lines[line].lower()
-    stations = get_raw_stations()
-    resp = []
-    for s in stations:
-        line_flag, stop_name, stop_id = s[station_code], s['stop_name'], s['stop_id']
-        if line_flag:
-            resp.append({'stop_id':stop_id, 'stop_name':stop_name,})
+def get_line_id(line_name):
+    lines = get_lines()
+    for key in lines.keys():
+        if line_name.lower() == key.lower():
+            return lines[key]
+    raise ValueError('Line not found: {}'.format(line_name))
+
+def get_lines():
+    """Returns CTA Format of thier Line for API"""
+    return {
+        'red':'Red',
+        'blue':'Blue',
+        'green':'G',
+        'brown':'Brn',
+        'purple':'P',
+        'yellow':'Y',
+        'pink':'Pnk',
+        'orange':'O'
+    }
+
+def get_line_stops(req_line):
+    """Returns a list of stops for a given line"""
+    line_id = get_line_id(req_line).lower()
+    if not line_id:
+        raise ValueError('Line not found: {}'.format(req_line))
+    raw_stops = get_raw_stations()
+    stops = []
+    for stop in raw_stops:
+        if stop[line_id]:
+            stops.append(stop)
+    return stops
+
+def get_raw_line_station_names(req_line):
+    """Returns a list of station names for a given line"""
+    stops = get_line_stops(req_line)
+    names = []
+    [names.append(tmp['station_name']) for tmp in stops]
+    # return a deduped list, each station has two stops
+    return list(set(names))
+
+def get_raw_arrivals(line_name,station_name):
+    """Returns upcoming arrivals for a given station and line"""
+    line_name = line_name.lower()
+    station_name = station_name.lower()
+    raw_stops = get_line_stops(line_name)
+    # find stop by matching name
+    station_id = False
+    for stop in raw_stops:
+        if station_name == stop['station_name'].lower():
+            station_id = stop['map_id']
+            break
+
+    if not station_id:
+        raise ValueError('station not found: {}'.format(station_name))
+    # load API key
+    with open("config.yml", 'r') as yml:
+        cfg = yaml.load(yml)
+    api_key = cfg['api']['key']
+
+    # this will have all lines for a particular station
+    raw_arrivals = requests.get('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={}&mapid={}&max=20&outputType=JSON'
+        .format(api_key, station_id)).json()
+
+    # filter out arrivals for trains of a different line color
+    line_id = get_line_id(line_name)
+    # orange line is the only one that uses a different ID here than the other API
+    if line_id == 'O': line_id = 'Org'
+    arrivals = []
+    for arrival in raw_arrivals['ctatt']['eta']:
+        if arrival['rt'] == line_id:
+            arrivals.append(arrival)
+
+    return arrivals
+
+def get_term_lines():
+    raw_lines = get_lines()
+    resp = '-' * 35
+    resp += '\n'
+    resp += 'CTA TRAIN LINES\n'
+    resp += '-' * 35
+    lines = raw_lines.keys()
+    lines.sort()
+    for line in lines:
+        resp += '\n + {}'.format(line.title())
+    resp += '\n'
     return resp
 
-def get_station(station_id):
-    '''to-do'''
-    return
+def get_term_line_stations(line_name):
+    stations = get_raw_line_station_names(line_name)
+    resp = '-' * 35
+    resp += '\nCTA {} LINE STATIONS\n'.format(line_name.upper())
+    resp += '-' * 35
+    stations.sort()
+    for sta in stations:
+        resp += '\n + {}'.format(sta)
+    resp += '\n'
+    resp += '-' * 35
+    resp += '\n'
+    return resp
 
-def get_arrivals_stop(api_key, station_id):
-    return requests.get('http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={}&stpid={}&max=20&outputType=JSON'.format(api_key, station_id)).json()
+def get_term_arrivals(line_name, station_name):
+
+    header = 'CTA {} LINE @ {}'.format(line_name.upper(), station_name.upper())
+    resp = '-' * 35
+    resp += '\n'
+    resp += header + '\n'
+    resp += '-' * 35
+
+    # build a dictionary of arrival strings
+    # if this raises an error for the station not existing, return the list
+    # of stations to the user instead
+    try:
+        raw_arrivals = get_raw_arrivals(line_name=line_name, station_name=station_name)
+    except ValueError:
+        return get_term_line_stations(line_name)
+    json.dumps(raw_arrivals)
+
+    stops = {}
+    for raw_arrival in raw_arrivals:
+        dest = raw_arrival['destNm']
+        if dest not in stops.keys():
+            stops[dest] = []
+        # build a string for each arrival
+        t = datetime.strptime(raw_arrival['arrT'], "%Y-%m-%dT%H:%M:%S")
+        time = t.strftime("%I:%M:%S")
+        elapsed = (t - datetime.now()).seconds
+        minutes = elapsed / 60
+        seconds = elapsed % 60
+        tmp = '\n - {}m {}s from now ({})'.format(minutes, seconds, time)
+        stops[dest].append(tmp)
+
+    for dest,arrivals in stops.items():
+        resp += '\n{} ---> {}'.format(raw_arrivals[0]['staNm'], dest)
+        for a in arrivals:
+            resp += a
+
+    resp += '\n'
+    resp += '-' * 35
+    resp += '\n'
+    return resp
 
 
 if __name__ == "__main__":
-    # setup - grab api key from config file
-    with open("config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
-    api_key =  cfg['api']['key']
 
-    stops = get_stops('Pink')
-    for s in stops:
-        print s
+    if len(sys.argv) == 1:
+        print 'usage:\n  ./cta.py <line>'
+        print '  ./cta.py <lime> <station_name>'
+        print 'examples:\n  ./cta.py brown'
+        print '  ./cta.py blue lasalle\n'
+        print get_term_lines()
+        exit(1)
 
-# Station IDs http://www.transitchicago.com/developers/ttdocs/default.aspx#_Toc296199909
+    if len(sys.argv) == 2:
+        print get_term_line_stations(sys.argv[1])
+
+    if len(sys.argv) == 3:
+        print get_term_arrivals(line_name=sys.argv[1], station_name=sys.argv[2])
